c1cec805636873c19ea239bceb5af6fcc810d312
Chen ZH <18453829+ChrisIllusion@users.noreply.github.com>
2017-12-01 16:48:48 +0800

    test1

---
+++ insert_sort.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int insert_sort(int *a, int length )
+{
+    for(int i=1; i<length; i++)
+    {
+		int temp = a[i];
+		int j = i - 1;
+		while (j>=0 && a[j]>temp)
+		{
+			a[j + 1] = a[j];
+			j = j - 1;
+		}
+		a[j + 1] = temp;
+
+    }
+    return 0;
+}
+
+int main()
+{
+    int s[10];
+    for(int i=0;i<10;i++)
+    {
+        s[i]=10-i;
+    }
+	//cout << sizeof(s) << endl;
+	cout << "before sort: ";
+	for (int i = 0; i<10; i++)
+	{
+		cout << s[i] << " ";
+	}
+	cout << endl;
+
+    insert_sort(s,10);
+
+	cout << "after sort: ";
+    for(int i=0;i<10;i++)
+    {
+        cout<<s[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
